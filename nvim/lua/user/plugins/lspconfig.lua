03789121f5b044461b2363c7b6d30715b5019fa8
Jay Tyrrell <tyrrell.jay@gmail.com>
2023-01-07 21:57:01 -0500

    Add description to some keymaps

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -1,12 +1,10 @@
-local opts = { noremap=true }
+local opts = { noremap = true }
 vim.keymap.set('n', '<leader>o', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
 local on_attach = function(_, bufnr)
-  local bufopts = { noremap=true, buffer=bufnr }
-  -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  local bufopts = { noremap = true, buffer = bufnr }
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
@@ -14,7 +12,7 @@ local on_attach = function(_, bufnr)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  -- vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -23,12 +21,12 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 --   on_attach = on_attach
 -- }
 
-require('lspconfig')['phpactor'].setup{
+require('lspconfig')['phpactor'].setup {
   on_attach = on_attach,
   capabilities = capabilities
 }
 
-require('lspconfig')['sumneko_lua'].setup{
+require('lspconfig')['sumneko_lua'].setup {
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
@@ -39,7 +37,7 @@ require('lspconfig')['sumneko_lua'].setup{
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
+        globals = { 'vim' },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -2,13 +2,12 @@ require('telescope').setup{}
 require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
-local bufopts = { noremap=true }
 
-vim.keymap.set('n', '<leader>d', builtin.lsp_definitions, bufopts)
-vim.keymap.set('n', '<leader>s', builtin.grep_string, bufopts)
-vim.keymap.set('n', '<leader>f', builtin.find_files, bufopts)
-vim.keymap.set('n', '<leader>g', builtin.live_grep, bufopts)
-vim.keymap.set('n', '<leader>b', builtin.buffers, bufopts)
-vim.keymap.set('n', '<leader>r', builtin.lsp_document_symbols, bufopts)
-vim.keymap.set('n', '<leader>m', builtin.keymaps, bufopts)
-vim.keymap.set('n', '<leader>ev', function() builtin.find_files({ cwd = '~/.config/nvim' }) end, bufopts)
+vim.keymap.set('n', '<leader>d', builtin.lsp_definitions, { noremap=true, desc="telescope.builtin.lsp_definitions" })
+vim.keymap.set('n', '<leader>s', builtin.grep_string, { noremap = true, desc = "telescope.builtin.grep_string" })
+vim.keymap.set('n', '<leader>f', builtin.find_files, { noremap = true, desc = "telescope.builtin.find_files" })
+vim.keymap.set('n', '<leader>g', builtin.live_grep, { noremap = true, desc = "telescope.builtin.live_grep" })
+vim.keymap.set('n', '<leader>b', builtin.buffers, { noremap = true, desc = "telescope.builtin.buffers" })
+vim.keymap.set('n', '<leader>r', builtin.lsp_document_symbols, { noremap = true, desc = "telescope.builtin.lsp_document_symbols" })
+vim.keymap.set('n', '<leader>m', builtin.keymaps, { noremap = true, desc = "telescope.builtin.keymaps" })
+vim.keymap.set('n', '<leader>ev', function() builtin.find_files({ cwd = '~/.config/nvim' }) end, { noremap = true, desc = "telescope.builtin.find_files for nvim config" })
