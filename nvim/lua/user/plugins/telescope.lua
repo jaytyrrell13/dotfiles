e07f4014c922998fbbc035cde6d06b9a7d334acc
Jay Tyrrell <tyrrell.jay@gmail.com>
2023-02-10 18:30:55 -0500

    Update keymap for artisan

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -31,4 +31,4 @@ vim.keymap.set('n', '<leader>m', builtin.keymaps, { noremap = true, desc = 'tele
 vim.keymap.set('n', '<leader>ev', function()
   builtin.find_files({ cwd = '~/.config/nvim' })
 end, { noremap = true, desc = 'telescope.builtin.find_files for nvim config' })
-vim.keymap.set('n', '<leader>a', ':Telescope artisan<cr>')
+vim.keymap.set('n', '<leader>a', extensions.artisan.artisan, { noremap = true, desc = 'telescope.extensions.artisan' })
