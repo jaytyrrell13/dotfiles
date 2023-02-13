326425c3b86d5b805474ae9916937cbd6f298658
Jay Tyrrell <tyrrell.jay@gmail.com>
2023-02-13 17:43:50 -0500

    Remove trailing whitespace for all file types

--- nvim/lua/user/misc.lua
+++ nvim/lua/user/misc.lua
@@ -1,8 +1,6 @@
 vim.cmd([[
   augroup RemoveTrailingSpaces
     autocmd!
-    autocmd BufWritePre *.php :%s/\s\+$//e
-    autocmd BufWritePre *.js :%s/\s\+$//e
-    autocmd BufWritePre *.vue :%s/\s\+$//e
+    autocmd BufWritePre * :%s/\s\+$//e
   augroup END
 ]])
