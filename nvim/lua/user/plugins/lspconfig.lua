a21477d88204db231e84ffe8614390b3c9f6c816
Jay Tyrrell <tyrrell.jay@gmail.com>
2023-02-13 17:12:08 -0500

    Rename lua language server

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -29,7 +29,7 @@ end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require('lspconfig')['phpactor'].setup({
+require('lspconfig').phpactor.setup({
   on_attach = on_attach,
   capabilities = capabilities,
 })
@@ -44,7 +44,7 @@ require('lspconfig').jsonls.setup({
   },
 })
 
-require('lspconfig')['sumneko_lua'].setup({
+require('lspconfig').lua_ls.setup({
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
