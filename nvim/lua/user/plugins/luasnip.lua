2bfa0e74f82052fdcdef2c5cd395b576d21f7137
Jay Tyrrell <tyrrell.jay@gmail.com>
2023-04-26 18:53:14 -0400

    Add friendly snippets

--- nvim/lua/user/plugins/luasnip.lua
+++ nvim/lua/user/plugins/luasnip.lua
@@ -1,5 +1,12 @@
 return {
   'L3MON4D3/LuaSnip',
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    config = function()
+      require('luasnip.loaders.from_vscode').lazy_load()
+      require('luasnip').filetype_extend('typescript', { 'javascript' })
+    end,
+  },
   config = function()
     local ls = require('luasnip')
 
