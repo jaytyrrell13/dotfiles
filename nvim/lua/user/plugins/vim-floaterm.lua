e6941a3c53ba9b89d9dc48eb80103c5be04745e3
Jay Tyrrell <tyrrell.jay@gmail.com>
2023-09-20 14:28:46 -0400

    Add floaterm plugin

---
+++ nvim/lua/user/plugins/vim-floaterm.lua
@@ -0,0 +1,17 @@
+return {
+  'voldikss/vim-floaterm',
+  cmd = { 'FloatermToggle' },
+  keys = {
+    { '<leader>T', ':FloatermToggle<cr>', mode = 'n' },
+    { '<leader>T', '<C-\\><C-n>:FloatermToggle<cr>', mode = 't' },
+  },
+  config = function()
+    vim.cmd([[
+      let g:floaterm_width = 0.8
+      let g:floaterm_height = 0.8
+
+      " FG Color from onenord
+      let g:terminal_color_0 = '#2e3440'
+    ]])
+  end,
+}
