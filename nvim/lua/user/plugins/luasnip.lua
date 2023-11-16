81e627c1b7befbea32845541c8d3973d68dce9a0
Jay Tyrrell <tyrrell.jay@gmail.com>
2023-11-16 08:28:30 -0500

    Update luasnip config

--- nvim/lua/user/plugins/luasnip.lua
+++ nvim/lua/user/plugins/luasnip.lua
@@ -2,12 +2,12 @@ return {
   'L3MON4D3/LuaSnip',
   dependencies = {
     'rafamadriz/friendly-snippets',
-    config = function()
-      require('luasnip.loaders.from_vscode').lazy_load()
-      require('luasnip').filetype_extend('typescript', { 'javascript' })
-    end,
+    'jaytyrrell13/static.nvim',
   },
   config = function()
+    require('luasnip.loaders.from_vscode').lazy_load()
+    require('luasnip').filetype_extend('typescript', { 'javascript' })
+
     local ls = require('luasnip')
 
     ls.config.set_config({
