f83f51cd53502cc76297767873f4a01045b6d559
Jay Tyrrell <tyrrell.jay@gmail.com>
2024-01-11 08:41:41 -0500

    Update keymap for code actions

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -34,7 +34,7 @@ return {
         end, opts)
         vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>a', vim.lsp.buf.code_action, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>C', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
       end,
     })
