f5a4d4f5e58061f8047131d29e10f149e86b9850
Jay Tyrrell <tyrrell.jay@gmail.com>
2024-12-27 10:56:42 -0500

    feat: turn off lazy notifications

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,7 +16,14 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require('lazy').setup('user.plugins', {
+require('lazy').setup({
+  spec = {
+    { import = 'user.plugins' },
+  },
+  change_detection = {
+    enabled = false,
+    notify = false,
+  },
   dev = {
     path = '~/Code',
     patterns = { 'jaytyrrell13' },
