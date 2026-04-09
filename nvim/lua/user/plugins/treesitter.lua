6e093c5f7fb8a884f9a2437cd5589de6d8a38703
Jay Tyrrell <tyrrell.jay@gmail.com>
2026-04-09 17:13:50 -0400

    refactor: use main branch for treesitter

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
   build = function()
     require('nvim-treesitter.install').update({ with_sync = true })
   end,
@@ -7,81 +8,92 @@ return {
     'JoosepAlviste/nvim-ts-context-commentstring',
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      ensure_installed = {
-        'astro',
-        'comment',
-        'css',
-        'dockerfile',
-        'git_config',
-        'git_rebase',
-        'gitcommit',
-        'gitignore',
-        'go',
-        'gomod',
-        'gosum',
-        'html',
-        'javascript',
-        'json',
-        'lua',
-        'markdown',
-        'markdown_inline',
-        'php',
-        'query',
-        'ruby',
-        'sql',
-        'twig',
-        'typescript',
-        'vue',
-        'xml',
-      },
-      highlight = {
-        -- 'false' will disable the whole extension
+  opts = {
+    textobjects = {
+      select = {
         enable = true,
+        lookahead = true,
+        keymaps = {
+          ['if'] = '@function.inner',
+          ['af'] = '@function.outer',
+          ['ia'] = '@parameter.inner',
+          ['aa'] = '@parameter.outer',
+          ['ic'] = '@class.inner',
+          ['ac'] = '@class.outer',
+        },
       },
-      indent = {
+      move = {
         enable = true,
-      },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ['if'] = '@function.inner',
-            ['af'] = '@function.outer',
-            ['ia'] = '@parameter.inner',
-            ['aa'] = '@parameter.outer',
-            ['ic'] = '@class.inner',
-            ['ac'] = '@class.outer',
-          },
+        set_jumps = true,
+        goto_next_start = {
+          [']m'] = '@function.outer',
         },
-        move = {
-          enable = true,
-          set_jumps = true,
-          goto_next_start = {
-            [']m'] = '@function.outer',
-          },
-          goto_next_end = {
-            [']M'] = '@function.outer',
-          },
-          goto_previous_start = {
-            ['[m'] = '@function.outer',
-          },
-          goto_previous_end = {
-            ['[M'] = '@function.outer',
-          },
+        goto_next_end = {
+          [']M'] = '@function.outer',
         },
-        swap = {
-          enable = true,
-          swap_next = {
-            ['<leader>a'] = '@parameter.inner',
-          },
-          swap_prev = {
-            ['<leader>A'] = '@parameter.inner',
-          },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
         },
       },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_prev = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+    },
+  },
+  init = function()
+    local ensureInstalled = {
+      'astro',
+      'comment',
+      'css',
+      'dockerfile',
+      'git_config',
+      'git_rebase',
+      'gitcommit',
+      'gitignore',
+      'go',
+      'gomod',
+      'gosum',
+      'html',
+      'javascript',
+      'json',
+      'lua',
+      'markdown',
+      'markdown_inline',
+      'php',
+      'query',
+      'ruby',
+      'sql',
+      'twig',
+      'typescript',
+      'vue',
+      'xml',
+    }
+
+    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+    local parsersToInstall = vim
+      .iter(ensureInstalled)
+      :filter(function(parser)
+        return not vim.tbl_contains(alreadyInstalled, parser)
+      end)
+      :totable()
+    require('nvim-treesitter').install(parsersToInstall)
+
+    vim.api.nvim_create_autocmd('FileType', {
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
